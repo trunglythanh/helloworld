5ee74a9be1b77789e33f11797262d2ca5576deae
trunglythanh <trunglythanh@gmail.com>
2021-12-31 06:54:30 +0700

    Commit to firsttestbranch

--- Helloworld/src/org/r2s/HelloWorldApp.java
+++ Helloworld/src/org/r2s/HelloWorldApp.java
@@ -3,6 +3,6 @@ package org.r2s;
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from R2s");
+		System.out.println("Hello from First test branch");
 	}
 }
